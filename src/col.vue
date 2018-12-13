6591d9eaf3a096361f7c7dd73b686318f53b7a2d
huazhen <1282038751@qq.com>
2018-12-13 18:00:21 +0800

    row/col雏形

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col" :class="[`col_${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'Gollum-col',
+        props: {
+            span: {
+                type: [Number,String]
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .col{
+        height:100px;
+        background: grey;
+        width:50%;
+        border: 1px solid red;
+    }
+    $class-prefix: col_;
+    // loops through 100 times
+    @for $n from 1 through 24 {
+        // for each $col_#{i}
+        .#{$class-prefix}#{$n} {
+            width: ($n / 24) * 100%;
+        }
+    }
+</style>
