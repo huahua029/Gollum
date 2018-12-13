d75c25d2b279c1593e0a009694bc59708d2e67dd
huazhen <1282038751@qq.com>
2018-12-13 21:04:56 +0800

    基本实现row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,31 +1,54 @@
 <template>
-    <div class="col" :class="[`col_${span}`]">
-        <slot></slot>
+    <div :class="[span && `col-${span}`,offset && `offset-${offset}`]" :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}"
+         class="col"
+    >
+        <div style="border: 1px solid green;height: 100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
-    export default{
-        name: 'Gollum-col',
+    export default {
+        name: 'GollumCol',
         props: {
             span: {
-                type: [Number,String]
+                type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
             }
+        },
+        data(){
+            return {
+                gutter: 0
+            }
+        },
+        created() {
+            console.log('col created')
+        },
+        mounted() {
+            console.log('col mounted')
         }
     }
 </script>
 <style lang="scss" scoped>
-    .col{
-        height:100px;
-        background: grey;
-        width:50%;
-        border: 1px solid red;
+    .col {
+        height: 100px;
+        width: 50%;
     }
-    $class-prefix: col_;
-    // loops through 100 times
+
+    $class-prefix: col-;
     @for $n from 1 through 24 {
-        // for each $col_#{i}
         .#{$class-prefix}#{$n} {
             width: ($n / 24) * 100%;
         }
     }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+        .#{$class-prefix}#{$n} {
+            margin-left: ($n / 24) * 100%;
+        }
+    }
+
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,28 @@
 <template>
-    <div class="row">
+    <div :style="{marginLeft: -gutter/2+'px',marginRight:-gutter/2+'px'}" class="row">
         <slot></slot>
     </div>
 </template>
+<script>
+    export default {
+        name: 'GollumRow',
+        props: {
+            gutter: {
+                type: [Number,String]
+            }
+        },
+        created() {
+            console.log('row created')
+        },
+        mounted() {
+            console.log('row mounted')
+            console.log(this.$children)
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
 <style lang="scss" scoped>
     .row{
         display: flex;
