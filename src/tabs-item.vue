23216ee2986d20c9025a6a57bafccd0c2afd71b4
huazhen <1282038751@qq.com>
2018-12-19 16:55:45 +0800

    触发update:selected事件的时候，添加item数据

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -36,22 +36,23 @@
         },
         methods: {
             xxx() {
-                this.eventBus.$emit('update:selected', this.name)
+                this.eventBus.$emit('update:selected', this.name,this)
             }
         }
     }
 </script>
 <style scoped lang="scss">
+    $blue: blue;
     .tabs-item {
         flex-shrink: 0;
         padding: 0 2em;
         cursor: pointer;
-        border: 1px solid red;
         display: flex;
         align-items: center;
         height: 100%;
         &.active{
-            background: red;
+            color: $blue;
+            font-weight: bold;
         }
     }
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -4,40 +4,50 @@
     </div>
 </template>
 <script>
-    import Vue from 'vue'
+  import Vue from 'vue'
 
-    export default {
-        name: 'GullumTabs',
-        props: {
-            selected: {
-                type: String,
-                required: true
-            },
-            direction: {
-                type: String,
-                default: 'horizontal',
-                validator(value){
-                    return ['horizontal','vertical'].indexOf(value) >=  0
-                }
-            }
-        },
-        data(){
-            return {
-                eventBus: new Vue()
-            }
-        },
-        provide(){
-            return {
-                eventBus: this.eventBus
-            }
-        },
-        mounted() {
-            this.eventBus.$emit('update:selected',this.selected)
+  export default {
+    name: 'GullumTabs',
+    props: {
+      selected: {
+        type: String,
+        required: true
+      },
+      direction: {
+        type: String,
+        default: 'horizontal',
+        validator(value) {
+          return ['horizontal', 'vertical'].indexOf(value) >= 0
         }
+      }
+    },
+    data() {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide() {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted() {
+      console.log(this.$children)
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'GullumTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'GullumTabsItem'
+              && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
     }
+  }
 </script>
 <style>
-    .tabs{
+    .tabs {
 
     }
 </style>
