a687375edf6716309cf6608e58ab5eff997550a7
huazhen <1282038751@qq.com>
2018-12-19 14:47:29 +0800

    添加tabs相关组件，添加基本props

---
+++ src/tabs-item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="tabs-itme">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GullumTabsItem',
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+<style>
+    .tabs-item{
+
+    }
+</style>
