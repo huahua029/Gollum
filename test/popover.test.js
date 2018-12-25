b9c08f9317e347f5f646ef57358c846482b08bf8
huazhen <1282038751@qq.com>
2018-12-25 11:49:23 +0800

    popover第一个测试用例

---
+++ test/popover.test.js
@@ -0,0 +1,41 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+  it('存在.', () => {
+    expect(Popover).to.be.exist
+  })
+  it('可以设置position.', (done) => {
+    Vue.component('g-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+            <g-popover position="bottom" ref="a">
+                <template slot="content">
+                    <div>popover内容</div>
+                </template>
+            <button>点我</button>
+    </g-popover>
+        `
+    const vm = new Vue({
+      el: div
+    })
+    vm.$nextTick(()=>{
+      console.log(vm.$el.outerHTML);
+      vm.$el.querySelector('button').click()
+      vm.$nextTick(()=>{
+        let {contentWrapper} = vm.$refs.a.$refs
+        expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+        done()
+      })
+    })
+    // const useElement = vm.$el.querySelector('use')
+    // expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+    // vm.$destroy()
+  })
+
+})
